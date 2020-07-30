e099da2aa3294c3cab18142f5d732524b548a970
“suresh” <esuresh1991@gmail.com>
2020-07-30 11:21:31 +0530

    cosdsd

--- Sur/src/Simple.java
+++ Sur/src/Simple.java
@@ -4,7 +4,9 @@ public class Simple {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hi how r");
-System.out.println("2");		
+System.out.println("2");
+System.out.println("3");
+
 	}
 
 }
